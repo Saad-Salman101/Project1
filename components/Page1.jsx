b155e0d61bc06ad2e5eef0dd109c98b375e4c95a
Saad <saad.salman98@gmail.com>
2023-05-18 18:30:50 +0500

    1st commit

--- components/Page1.jsx
+++ components/Page1.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 const Page1 = () => {
   return (
     <>
-      <div className='h-screen w-full flex-col justify-between'>
+      <div className='h-screen w-full flex-col justify-between relative'>
         <div className='w-full h-[30%]'></div>
         <div className='md:w-[30%] w-[50%] h-[30%] flex justify-start items-center ml-4'>
           <img src='/images/P1ZimoGroup.png' alt='' />
         </div>
         <div className='w-full h-[30%] flex justify-center items-end'>
-
+              <img src='/images/P1BackLogo.png' alt='' className='absolute bottom-1/4 opacity-40' />
             <a href='#page2'>
               <img src='/images/DownArrow.png' alt='' />
             </a>
--- components/Page4.jsx
+++ components/Page4.jsx
@@ -5,27 +5,27 @@ import Link from 'next/link'
 const Page4 = () => {
   return (
     <>
-<div id="page4" className='h-screen flex flex-col  justify-between items-center bg-[url("/images/b5.png")] bg-contain bg-center bg-no-repeat bg-black'>
+      <div id="page4" className='h-screen flex flex-col  justify-between items-center bg-[url("/images/b5.png")] bg-contain bg-center bg-no-repeat bg-black'>
 
-<div className='h-[35%] w-full'>
-  
-</div>
-<div className='h-[30%] w-full'>
-  
-</div>
-<div className='h-[35%] w-full flex flex-col justify-end items-center'>
-<Link href='#page5'  >
+        <div className='h-[35%] w-full'>
+
+        </div>
+        <div className='h-[30%] w-full flex items-center justify-center'>
+        <img src='/images/P4Zimo2.png' alt=''  className='opacity-40'/>
+        </div>
+        <div className='h-[35%] w-full flex flex-col justify-end items-center'>
+          <Link href='#page5'  >
             <a>
               <img src='/images/DownArrow.png' alt='' />
             </a>
           </Link>
-</div>
+        </div>
 
 
-</div>
+      </div>
 
 
-      
+
     </>
   )
 }
